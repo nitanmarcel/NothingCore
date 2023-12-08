59268daa83dddc40896cabecff92a2857a6370f7
Marcel Alexandru Nitan <nitan.marcel@gmail.com>
2023-12-08 18:43:42 +0200

    Rewrite overlay logic

--- module/customize.sh
+++ module/customize.sh
@@ -3,3 +3,21 @@ sdk_version=$(getprop ro.build.version.sdk)
 ui_print "Installing for $sdk_version"
 
 cp -af "$MODPATH/$sdk_version"/* "$MODPATH/"
+
+OVERLAY_DIR="/system/vendor/overlay"
+
+if [ -d "/system/vendor/overlay" ]; then
+    OVERLAY_DIR="/system/vendor/overlay"
+elif [ -d "/system/product/overlay" ]; then
+    OVERLAY_DIR="/system/product/overlay"
+elif [ -d "/system/system_ext/overlay" ]; then
+    OVERLAY_DIR="/system/system_ext/overlay"
+else
+    echo "Could not find a suitable overlay directory."
+    exit 1
+fi
+
+ui_print "Installing overlay in $OVERLAY_DIR"
+
+mkdir -p $MODPATH/$OVERLAY_DIR
+cp -af $MODPATH/$sdk_version/overlay/* $MODPATH/$OVERLAY_DIR/ || true
